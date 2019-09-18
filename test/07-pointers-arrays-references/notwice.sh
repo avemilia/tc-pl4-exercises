bde803cab75d11a439496c47d439bd7473e0845c
Ave Milia <avemilia@protonmail.com>
2019-09-18 17:22:44 +0200

    Add more tests for 07-pointers-arrays-references_notwice

--- test/07-pointers-arrays-references/notwice.sh
+++ test/07-pointers-arrays-references/notwice.sh
@@ -5,6 +5,30 @@ source test_common.sh # gets copied in bindir with this name
 target="07-pointers-arrays-references_notwice"
 
 function test_1() {
+    args="-a <<<"
+    input="abc 123 abc"
+    expect_output=""
+    expect_status=1
+    test::test "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_2() {
+    args="-s abc <<<"
+    input="abc 123 abc"
+    expect_output=""
+    expect_status=1
+    test::test "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_3() {
+    args="<<<"
+    input="abc 123 abc"
+    expect_output="Words: 'abc' '123'"
+    expect_status=0
+    test::test "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_4() {
     args="<<<"
     input="abc 123 abc Quit bcd"
     expect_output="Words: 'abc' '123'"
@@ -12,7 +36,7 @@ function test_1() {
     test::test "$target" "$args" "$input" "$expect_output" "$expect_status"
 }
 
-function test_2() {
+function test_5() {
     args="-s <<<"
     input="abc 123 abc Quit bcd"
     expect_output="Words: '123' 'abc'"
