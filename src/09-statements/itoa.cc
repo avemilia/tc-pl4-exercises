ddd2cc5980a67e6a5f67aa9c3b82a278c575b09d
Ave Milia <avemilia@protonmail.com>
2019-09-22 16:13:35 +0200

    Add support for negative ints to itoa, better testing

--- src/09-statements/itoa.cc
+++ src/09-statements/itoa.cc
@@ -7,23 +7,60 @@
 #include <cmath>
 #include <memory>
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-char* itoa(int i, char* b, long b_len) {
-    int num_of_digits = static_cast<int>(log10(i)) + 1;
-    if (b_len < num_of_digits + 1) // for '\0'
-        return nullptr;
-
+void itoa_helper(int i, char* b, int num_of_digits) {
     int n {num_of_digits};
     do {
         auto q_r = div(i, 10);
         i = q_r.quot;
         b[n-- - 1] = q_r.rem + '0';
     } while (i);
-
     b[num_of_digits] = '\0';
+}
 
+char* itoa(int i, char* b, long b_len) {
+    // log10 cannot be called with i <= 0, so deal with that manually
+
+    if (i == 0) {
+        if (b_len >= 2) {
+            b[0] = '0';
+            b[1] = '\0';
+            return b;
+        }
+        return nullptr;
+    }
+
+    if (i < 0) {
+        // calling abs() on INT_MIN is undefined
+        if (i == numeric_limits<int>::min()) {
+            i = numeric_limits<int>::max();
+            // this will suffice to count num_of_digits, but remember
+            // to add 1 to b[num_of_digits] later
+            int num_of_digits = static_cast<int>(log10(i)) + 1;
+            if (b_len < num_of_digits + 2) // for '-' and '\0'
+                return nullptr;
+            b[0] = '-';
+            itoa_helper(i, b + 1, num_of_digits);
+            b[num_of_digits]++;
+            return b;
+        }
+
+        i = abs(i);
+        int num_of_digits = static_cast<int>(log10(i)) + 1;
+        if (b_len < num_of_digits + 2) // for '-' and '\0'
+            return nullptr;
+        b[0] = '-';
+        itoa_helper(i, b + 1, num_of_digits);
+        return b;
+    }
+
+    int num_of_digits = static_cast<int>(log10(i)) + 1;
+    if (b_len < num_of_digits + 1) // for '\0'
+        return nullptr;
+    itoa_helper(i, b, num_of_digits);
     return b;
 }
 
@@ -36,7 +73,7 @@ int main(int argc, char** argv) {
     // no error checking because this is to test itoa() only
     int i = atoi(argv[1]);
     long b_len = atol(argv[2]);
-    unique_ptr<char[]> b {new char[b_len] {}};
+    unique_ptr<char[]> b {new char[b_len]};
     if (!itoa(i, b.get(), b_len))
         exit(2);
     cout << b.get() << '\n';
--- test/09-statements/itoa.sh
+++ test/09-statements/itoa.sh
@@ -15,9 +15,10 @@ function test_1() {
     test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
 }
 
+# zero
 function test_2() {
     env_args=""
-    args="123 1"
+    args="0 0"
     input=""
     expect_output=""
     expect_status=2
@@ -26,7 +27,7 @@ function test_2() {
 
 function test_3() {
     env_args=""
-    args="123 3"
+    args="0 1"
     input=""
     expect_output=""
     expect_status=2
@@ -35,6 +36,43 @@ function test_3() {
 
 function test_4() {
     env_args=""
+    args="0 2"
+    input=""
+    expect_output="0"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_5() {
+    env_args=""
+    args="0 20"
+    input=""
+    expect_output="0"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+# positive integer, normal
+function test_6() {
+    env_args=""
+    args="123 0"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_7() {
+    env_args=""
+    args="123 3"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_8() {
+    env_args=""
     args="123 4"
     input=""
     expect_output="123"
@@ -42,11 +80,140 @@ function test_4() {
     test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
 }
 
-function test_5() {
+function test_9() {
     env_args=""
-    args="123 5"
+    args="123 20"
     input=""
     expect_output="123"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+# INT_MAX
+function test_10() {
+    env_args=""
+    int_max=$(getconf INT_MAX)
+    args="$int_max 0"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_11() {
+    env_args=""
+    int_max=$(getconf INT_MAX)
+    int_max_len=$(getconf INT_MAX | wc -c)
+    int_max_len=$((int_max_len-1))
+    args="$int_max $int_max_len"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_12() {
+    env_args=""
+    int_max=$(getconf INT_MAX)
+    int_max_len=$(getconf INT_MAX | wc -c)
+    args="$int_max $int_max_len"
+    input=""
+    expect_output="$int_max"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_13() {
+    env_args=""
+    int_max=$(getconf INT_MAX)
+    int_max_len=$(getconf INT_MAX | wc -c)
+    int_max_len=$((int_max_len + 20))
+    args="$int_max $int_max_len"
+    input=""
+    expect_output="$int_max"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+# negative integer, normal
+function test_14() {
+    env_args=""
+    args="-123 0"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_15() {
+    env_args=""
+    args="-123 4"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_16() {
+    env_args=""
+    args="-123 5"
+    input=""
+    expect_output="-123"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_17() {
+    env_args=""
+    args="-123 20"
+    input=""
+    expect_output="-123"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+# INT_MIN
+function test_18() {
+    env_args=""
+    int_min=$(getconf INT_MIN)
+    args="$int_min 0"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_19() {
+    env_args=""
+    int_min=$(getconf INT_MIN)
+    int_min_len=$(getconf INT_MIN | wc -c)
+    int_min_len=$((int_min_len - 1))
+    args="$int_min $int_min_len"
+    input=""
+    expect_output=""
+    expect_status=2
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_20() {
+    env_args=""
+    int_min=$(getconf INT_MIN)
+    int_min_len=$(getconf INT_MIN | wc -c)
+    args="$int_min $int_min_len"
+    input=""
+    expect_output="$int_min"
+    expect_status=0
+    test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
+}
+
+function test_21() {
+    env_args=""
+    int_min=$(getconf INT_MIN)
+    int_min_len=$(getconf INT_MIN | wc -c)
+    int_min_len=$((int_min_len + 20))
+    args="$int_min $int_min_len"
+    input=""
+    expect_output="$int_min"
     expect_status=0
     test::test "$env_args" "$target" "$args" "$input" "$expect_output" "$expect_status"
 }
